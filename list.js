fd84102e84541c32fcaf1527added78909ec9bce
jarirliu1986 <289374247@qq.com>
2016-09-18 12:15:34 -0400

    find element method added

--- list.js
+++ list.js
@@ -4,12 +4,26 @@ function List() {
 	this.pos = 0;		// the current postion in the list
 	this.dataStore = [];//initializes an empty array to store list elements
 	
-	this.append = append;
-	
-	// append: adding an element to a list
-	function append(element) {
-	 	this.dataStore[this.listSize++] = element;
+	this.append = append; // adding an element to a list
+	this.find = find; 	  // find an ele and return the index , if no return -1
+	this.remove = remove; //remove an element from a list
+}
+
+function append(element) {
+	this.dataStore[this.listSize++] = element;
+}
+
+function find(element) {
+	for(var i=0; i < this.listSize; i++){
+		if (element == this.dataStore[i]) {
+			return i;
+		}
 	}
+	return -1;
+}
+
+function remove(argument) {
+	// body...
 }
 	
 

11e0439c6fda35cf83df8eefb82e1e287d695980
jarirliu1986 <289374247@qq.com>
2016-09-18 11:39:30 -0400

    initial added

---
+++ list.js
@@ -0,0 +1,12 @@
+// Constructor function of the List
+function List() {
+	this.listSize = 0; // number of elements in the list
+	this.pos = 0;		// the current postion in the list
+	this.dataStore = [];//initializes an empty array to store list elements
+	this.append = append;
+}
+
+// append: adding an element to a list
+function append(element) {
+	
+}

b759f678ae61ecb4481bd01dc5c4d9d5372404c6
jarirliu1986 <289374247@qq.com>
2016-09-19 14:21:48 -0400

    add unit test

--- test/queue_spec.js
+++ test/queue_spec.js
@@ -30,5 +30,35 @@ describe("Queue", function () {
 		queue.put("a");
 		queue.put("b");
 		queue.put("aa");
+
+		var ele = queue.get();
+		expect(ele).toBe("a");
+		expect(queue.size()).toBe(2);
+		var ele1 = queue.get();
+		expect(ele1).toBe("b");
 	});
+
+	it("front", function () {
+		queue.put("a");
+		queue.put("b");
+		queue.put("aa");
+		var ele = queue.front();
+		expect(ele).toBe("a");
+		expect(queue.size()).toBe(3);
+	});
+
+	it("end", function () {
+		queue.put("a");
+		queue.put("b");
+		queue.put("aa");
+		var ele = queue.end();
+		expect(ele).toBe("aa");
+		expect(queue.size()).toBe(3);
+	});
+
+	it("isEmpty", function () {
+		expect(queue.isEmpty()).toBe(true);
+		queue.put("a");
+		expect(queue.isEmpty()).toBe(false);
+	})
 })

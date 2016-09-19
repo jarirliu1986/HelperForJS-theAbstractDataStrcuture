ac102f2df094432cc5644986926b20361564fea7
jarirliu1986 <289374247@qq.com>
2016-09-18 20:55:02 -0400

    11

---
+++ test/list_spec.js
@@ -0,0 +1,10 @@
+var List = require('../src/list');
+
+describe("List", function(){
+	it("exist", function(){
+		var list = new List();
+
+		expect(list).toBeDefined();
+	});
+
+});
---
+++ test/stack_spec.js
@@ -0,0 +1,9 @@
+var Stack = require('../src/stack');
+
+describe("Stack", function(){
+	it("exist", function(){
+		var stack = new Stack();
+		expect(stack).toBeDefined();
+	});
+
+});
